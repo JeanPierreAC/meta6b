124d146031d9e62949a628a4a4282f8dcd65bd31
JeanPierreAC <100950211+JeanPierreAC@users.noreply.github.com>
2025-04-21 23:21:32 -0500

    Update index.php

--- app/views/sexo/index.php
+++ app/views/sexo/index.php
@@ -30,7 +30,7 @@
                             <a href="/meta6b/sexo/edit?id=<?php echo htmlspecialchars($sexo['id']); ?>">
                                 <button>Editar</button>
                             </a>
-                            <a href="/meta6b/sexo/eliminar?id=<?php echo htmlspecialchars($sexo['id']); ?>" 
+                            <a href="/meta6b/public/sexo/eliminar?id=<?php echo htmlspecialchars($sexo['id']); ?>" 
                                onclick="return confirm('¿Estás seguro de eliminar este registro?');">
                                 <button>Eliminar</button>
                             </a>
